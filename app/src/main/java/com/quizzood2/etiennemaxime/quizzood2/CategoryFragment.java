90695531187ac5f8a2d2319675ca4f8512b3e019
s11705768 <Xdycfkgq9>
2018-06-06 14:45:49 +0200

    Fonctionnelle V1

--- app/src/main/java/com/quizzood2/etiennemaxime/quizzood2/CategoryFragment.java
+++ app/src/main/java/com/quizzood2/etiennemaxime/quizzood2/CategoryFragment.java
@@ -30,7 +30,7 @@ public class CategoryFragment extends Fragment {
     @android.support.annotation.Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @android.support.annotation.Nullable ViewGroup container, @android.support.annotation.Nullable Bundle savedInstanceState) {
-        myFragment = inflater.inflate(android.R.layout.fragment_category, container, false);
+        myFragment = inflater.inflate(R.layout.fragment_category, container, false);
         return myFragment;
     }
 }
--- app/src/main/java/com/quizzood2/etiennemaxime/quizzood2/Common/Common.java
+++ app/src/main/java/com/quizzood2/etiennemaxime/quizzood2/Common/Common.java
@@ -3,6 +3,6 @@ package com.quizzood2.etiennemaxime.quizzood2.Common;
 public class Common {
 
     public static String categoryId;
-    public static User currentUSer;
+//    public static User currentUSer;
 
 }
